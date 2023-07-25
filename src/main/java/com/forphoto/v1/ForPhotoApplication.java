c76fcfe261e2e7b29965d8c9171afb4b33a26d38
onlee1425 <enflenffl@gmail.com>
2023-07-25 17:15:42 +0900

    init

---
+++ src/main/java/com/forphoto/v1/ForPhotoApplication.java
@@ -0,0 +1,13 @@
+package com.forphoto.v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForPhotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForPhotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forphoto/v1/ForPhotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forphoto.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForPhotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
