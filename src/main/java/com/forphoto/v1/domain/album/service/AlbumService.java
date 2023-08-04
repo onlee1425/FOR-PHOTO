b268c24f0a09d4315f9a3a90eae70de299581da2
onlee1425 <enflenffl@gmail.com>
2023-08-04 18:52:58 +0900

    refactor: 앨범-앨범정보조회
    JPA 사용에 따른 Mapper 클래스 제거

--- src/main/java/com/forphoto/v1/domain/album/service/AlbumService.java
+++ src/main/java/com/forphoto/v1/domain/album/service/AlbumService.java
@@ -2,7 +2,6 @@ package com.forphoto.v1.domain.album.service;
 
 import com.forphoto.v1.domain.album.dto.AlbumInfoResponse;
 import com.forphoto.v1.domain.album.entity.Album;
-import com.forphoto.v1.domain.album.mapper.AlbumMapper;
 import com.forphoto.v1.domain.album.repository.AlbumRepository;
 import com.forphoto.v1.domain.photo.repository.PhotoRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +21,11 @@ public class AlbumService {
         Optional<Album> result = albumRepository.findById(albumId);
 
         if (result.isPresent()) {
-            AlbumInfoResponse response = AlbumMapper.convertToDto(result.get());
+            Album album = result.get();
+            AlbumInfoResponse response = new AlbumInfoResponse();
+            response.setAlbumId(album.getAlbumId());
+            response.setAlbumName(album.getAlbumName());
+            response.setCreatedAt(album.getCreatedAt());
             response.setCount(photoRepository.countByAlbum_AlbumId(albumId));
 
             return response;
