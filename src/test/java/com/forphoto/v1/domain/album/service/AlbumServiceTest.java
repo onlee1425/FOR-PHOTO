6a7efa40fd08c1afc06df324b90c62ebdd0cc247
onlee1425 <enflenffl@gmail.com>
2023-08-07 23:27:59 +0900

    test : 앨범생성 테스트

--- src/test/java/com/forphoto/v1/domain/album/service/AlbumServiceTest.java
+++ src/test/java/com/forphoto/v1/domain/album/service/AlbumServiceTest.java
@@ -1,17 +1,24 @@
 package com.forphoto.v1.domain.album.service;
 
 import com.forphoto.v1.domain.album.dto.AlbumInfoResponse;
+import com.forphoto.v1.domain.album.dto.CreateAlbumResponse;
 import com.forphoto.v1.domain.album.entity.Album;
 import com.forphoto.v1.domain.album.repository.AlbumRepository;
 import com.forphoto.v1.domain.photo.entity.Photo;
 import com.forphoto.v1.domain.photo.repository.PhotoRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @Transactional
@@ -54,5 +61,39 @@ class AlbumServiceTest {
         assertNotNull(resultAlbum);
         assertEquals("테스트", resultAlbum.getAlbumName());
         assertEquals(1, resultAlbum.getCount());
+    }
+
+    @Test
+    void createAlbum() {
+
+        // Given
+        String albumName = "테스트앨범";
+        Album mockAlbum = Album.builder()
+                .albumId(1L)
+                .albumName(albumName)
+                .createdAt(new Date())
+                .build();
+
+        // Mock the repository behavior
+        AlbumRepository albumRepositoryMock = mock(AlbumRepository.class);
+        when(albumRepositoryMock.findByAlbumName(albumName)).thenReturn(new ArrayList<>());
+        when(albumRepositoryMock.save(Mockito.any(Album.class))).thenReturn(mockAlbum);
+
+        PhotoRepository photoRepositoryMock = mock(PhotoRepository.class);
+        when(photoRepositoryMock.save(Mockito.any(Photo.class))).thenReturn(new Photo());
+
+        AlbumService albumService = new AlbumService(albumRepositoryMock, photoRepositoryMock);
+
+        // When
+        CreateAlbumResponse response = albumService.createAlbum(albumName);
+
+
+        // Then
+        assertNotNull(response);
+        assertEquals(mockAlbum.getAlbumId(), response.getAlbumId());
+        assertEquals(mockAlbum.getAlbumName(), response.getAlbumName());
+        assertEquals(mockAlbum.getCreatedAt(), response.getCreatedAt());
+        assertEquals(0, response.getCount());
+
     }
 }
