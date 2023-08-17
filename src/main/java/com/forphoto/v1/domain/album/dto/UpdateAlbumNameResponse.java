08322d294594771cf1c6b88c952f9243ce3059d5
onlee1425 <enflenffl@gmail.com>
2023-08-17 19:55:08 +0900

    feat : 앨범명 변경

---
+++ src/main/java/com/forphoto/v1/domain/album/dto/UpdateAlbumNameResponse.java
@@ -0,0 +1,16 @@
+package com.forphoto.v1.domain.album.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class UpdateAlbumNameResponse {
+
+    private Long albumId;
+    private String albumName;
+    private Date createdAt;
+    private int count;
+}
