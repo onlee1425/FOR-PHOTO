182247786a76ceab444126109c6300bdf096bace
onlee1425 <enflenffl@gmail.com>
2023-08-30 19:22:53 +0900

    feat : 회원역할 추가(일반회원,OAUTH회원)

--- src/main/java/com/forphoto/v1/security/springSecurity/UserDetail/CustomMemberDetails.java
+++ src/main/java/com/forphoto/v1/security/springSecurity/UserDetail/CustomMemberDetails.java
@@ -4,9 +4,12 @@ import com.forphoto.v1.domain.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @RequiredArgsConstructor
 public class CustomMemberDetails implements UserDetails {
@@ -15,7 +18,9 @@ public class CustomMemberDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(member.getMemberRole().name()));
+        return authorities;
     }
 
     @Override
